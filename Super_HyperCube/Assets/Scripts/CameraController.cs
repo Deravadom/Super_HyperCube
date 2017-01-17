693bb0521239e978417411e413fab6ec495fa5d0
Michael Needleman <mcn46@drexel.edu>
2017-01-17 15:56:45 -0500

    Initial setup and some work done

---
+++ Super_HyperCube/Assets/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject room;
+	public GameObject player;
+	private Vector3 center;
+
+	public bool isEnabled = true;
+
+	// Use this for initialization
+	void Start () {
+		if (!isEnabled)
+			return;
+		Transform[] wallsT = room.GetComponentsInChildren<Transform> ();
+		float[][] wallsV = new float[3][];
+
+		//if 2 transforms have 2 matching components, put them together
+		for (int i = 0; i < wallsT.Length-1; i++) {
+			for (int j = i+1; j < wallsT.Length; j++) {
+				bool xMatch = (wallsT [i].position.x == wallsT [j].position.x);
+				bool yMatch = (wallsT [i].position.y == wallsT [j].position.y);
+				bool zMatch = (wallsT [i].position.z == wallsT [j].position.z);
+
+				//I want to explicitly sort here
+				if(yMatch && zMatch) //x case
+					wallsV[0] = new float[2] {wallsT[i].position.x, wallsT[j].position.x};
+				else if(xMatch && zMatch) //y case
+					wallsV[1] = new float[2] {wallsT[i].position.y, wallsT[j].position.y};
+				else if(xMatch && yMatch) //z case
+					wallsV[2] = new float[2] {wallsT[i].position.z, wallsT[j].position.z};
+			}
+		}
+
+		//wallsV is now set up, time to find the center point;
+		center = new Vector3((wallsV[0][0] + wallsV[0][1])/2, (wallsV[1][0] + wallsV[1][1])/2, (wallsV[2][0] + wallsV[2][1])/2);
+		this.transform.position = center;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isEnabled)
+			return;
+		this.transform.LookAt (player.transform.position);
+	}
+}
---
+++ Super_HyperCube/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 20f;
+	public Camera cam;
+
+	private Vector3 camOffset;
+	private Rigidbody rigidBody;
+
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = this.GetComponent<Rigidbody> ();
+		camOffset = cam.transform.position - this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerMovement ();
+		updateCamera ();
+	}
+
+	private void playerMovement() {
+		Vector3 newVelocity = rigidBody.velocity;
+		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+			//rigidBody.velocity = new Vector3 (rigidBody.velocity.x, rigidBody.velocity.y, speed);
+			newVelocity.z = speed;
+		if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+			//rigidBody.velocity = new Vector3 (rigidBody.velocity.x, rigidBody.velocity.y, -speed);
+			newVelocity.z = -speed;
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+			//rigidBody.velocity = new Vector3 (-speed, rigidBody.velocity.y, rigidBody.velocity.z);
+			newVelocity.x= -speed;
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+			//rigidBody.velocity = new Vector3 (speed, rigidBody.velocity.y, rigidBody.velocity.z);
+			newVelocity.x = speed;
+		/*if (Input.GetKey (KeyCode.Space) && IsOnGround())
+			rigidBody.velocity = new Vector3 (rigidBody.velocity.x, jumpForce, rigidBody.velocity.z);*/
+		if (Input.GetKey (KeyCode.Space)) {
+			//rigidBody.velocity = new Vector3 (rigidBody.velocity.x, speed, rigidBody.velocity.z);
+			newVelocity.y = speed;
+		}
+		if(Input.GetKey(KeyCode.LeftShift)) {
+			//rigidBody.velocity = new Vector3 (rigidBody.velocity.x, -speed + UnityEngine.Physics.gravity.z, rigidBody.velocity.z);
+			newVelocity.y = -speed + UnityEngine.Physics.gravity.z;
+		}
+
+		rigidBody.velocity = newVelocity;
+	}
+	private bool IsOnGround() {
+		return Physics.Raycast(transform.position, Vector3.down, 0.51f);
+	}
+	private void updateCamera() {
+		cam.transform.position = (this.transform.position + camOffset);
+	}
+}
